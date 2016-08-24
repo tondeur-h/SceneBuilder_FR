7b914a8f18d50f8f76887dda473e31e82ad70734
herve <herve@pclmde>
2016-08-24 13:05:58 +0200

    Master-SceneBuilder-FR_20160824

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ cd ~/dev/HT-scenebuilder
 gradle assemble
 sudo mv /home/herve/dev/HT-scenebuilder/build/libs/scenebuilder-all-8.2.0-FRENCH-all.jar /opt/SceneBuilder/app/dist.jar
 
-pftp -n -v < deploy.ftp
-
 /opt/SceneBuilder/SceneBuilder &
+
+pftp -n -v < deploy.ftp
